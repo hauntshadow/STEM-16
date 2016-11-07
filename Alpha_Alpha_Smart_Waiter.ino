3178cf156927990768fceb3cd103e8a4eec581d9
martinezj002 <martinezj2@appstate.edu>
2016-11-07 15:51:31 -0500

    Update Alpha_Alpha_Smart_Waiter.ino

--- Alpha_Alpha_Smart_Waiter.ino
+++ Alpha_Alpha_Smart_Waiter.ino
@@ -4,7 +4,11 @@ Version 0.1
 const int sensorPin = A0;  // initialize light sensor to pin A0
 const int motorPin = 9;  // initialize motors to pin 9
 const int ledPin = 5;  // initialize leds to pin 5
-int sensorState; // holds light sensor value
+const int buttonPin = 0;
+int sensorState = 0; // holds light sensor value
+int lightRead = 0;
+int buttonState = 0;
+
 
 void setup()  {
   pinMode(sensorPin, INPUT);  // sets light sensor as input
@@ -12,10 +16,16 @@ void setup()  {
   pinMode(ledPin, OUTPUT);  // sets leds as output
   digitalWrite(ledPin, HIGH);  // turns on led
   Serial.begin(9600);  // starts communication through port
+  if(buttonState == HIGH)
+    {
+      lightRead = analogRead(sensorPin);
+      Serial.println(lightRead);
+      delay(100);
+    }
 }
 void loop()  {
   sensorState = analogRead(sensorPin);  // retrieves light sensor reading
-  if(sensorState >= 0 && sensorState <= 300)  {
+  if(sensorState >= sensorRead)  {
     digitalWrite(motorPin, HIGH);
   }
   else  {
